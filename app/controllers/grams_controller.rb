28c0616bfdd8e8a393755da8dea0dcd89bf076d1
Patrick Lyden <syncc12@verizon.net>
2019-09-27 16:21:48 +0000

    Added Validations.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,12 +1,16 @@
 class GramsController < ApplicationController
- 
+  
   def new 
     @gram = Gram.new
   end
 
   def create
     @gram = Gram.create(gram_params)
-    redirect_to root_path
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
