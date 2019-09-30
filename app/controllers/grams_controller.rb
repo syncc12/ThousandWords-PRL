689e4f550180fd864e3ed71ad5cfaaab56ceaa7f
Patrick Lyden <syncc12@verizon.net>
2019-09-30 16:06:00 +0000

    Added Gram Detail Page.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,7 +1,7 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  
+
   def new 
     @gram = Gram.new
   end
@@ -16,6 +16,13 @@ class GramsController < ApplicationController
   end
 
   def index
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   private
